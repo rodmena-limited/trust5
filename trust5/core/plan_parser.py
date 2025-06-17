eb3559828df769c2bccf7657f50af599d09de32c
Farshid Ashouri <farsheed.ashouri@gmail.com>
2025-06-17 20:21:28 +0100

    Add _EARS_TAG_RE to plan_parser

--- trust5/core/plan_parser.py
+++ trust5/core/plan_parser.py
@@ -5,3 +5,7 @@ from dataclasses import dataclass
 import yaml
 logger = logging.getLogger(__name__)
 _DEFAULT = PlanConfig()
+_EARS_TAG_RE = re.compile(
+    r"^\s*-\s*\[(UBIQ|EVENT|STATE|UNWNT|OPTNL|COMPLX)\]\s*(.+)",
+    re.IGNORECASE,
+)
