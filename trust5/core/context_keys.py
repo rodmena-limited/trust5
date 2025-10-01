b88d5b66f7a4eb6da1ee3c92e05fc9bb74708a8a
Farshid Ashouri <farsheed.ashouri@gmail.com>
2025-10-02 00:31:11 +0100

    Add PROPAGATED_CONTEXT_KEYS to context_keys

--- trust5/core/context_keys.py
+++ trust5/core/context_keys.py
@@ -3,3 +3,16 @@ import logging
 from typing import Any
 logger = logging.getLogger(__name__)
 DEFAULT_MAX_JUMPS = 50
+PROPAGATED_CONTEXT_KEYS: tuple[str, ...] = (
+    "jump_repair_ref",
+    "jump_validate_ref",
+    "jump_implement_ref",
+    "jump_quality_ref",
+    "test_files",
+    "owned_files",
+    "module_name",
+    "plan_config",
+    "repair_attempt",
+    "_max_jumps",
+    "_jump_count",
+)
