d0528a26d1c9329a0fcfa9c0134a31048bb3539d
Farshid Ashouri <farsheed.ashouri@gmail.com>
2025-10-03 19:04:32 +0100

    Initial mutation_task module structure

---
+++ trust5/tasks/mutation_task.py
@@ -0,0 +1,11 @@
+import logging
+import os
+import random
+import re
+import subprocess
+from dataclasses import dataclass
+from typing import Any
+from stabilize import StageExecution, Task, TaskResult
+from ..core.lang import LanguageProfile
+from ..core.message import M, emit
+logger = logging.getLogger(__name__)
