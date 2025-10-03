15bc95c77e71551365463c6b3617ca2e243888a8
Farshid Ashouri <farsheed.ashouri@gmail.com>
2025-10-03 21:14:45 +0100

    Add _QUOTED_RE to compliance

--- trust5/core/compliance.py
+++ trust5/core/compliance.py
@@ -6,3 +6,4 @@ from dataclasses import dataclass
 logger = logging.getLogger(__name__)
 _PASCAL_CASE_RE = re.compile(r"\b[A-Z][a-z]+(?:[A-Z][a-z]+)+\b")
 _BACKTICK_RE = re.compile(r"`([^`]+)`")
+_QUOTED_RE = re.compile(r'"([^"]+)"')
