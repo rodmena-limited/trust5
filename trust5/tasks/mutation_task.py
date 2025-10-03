1b90460d032f5079003d4bc99948487679bf7b06
Farshid Ashouri <farsheed.ashouri@gmail.com>
2025-10-03 19:11:29 +0100

    Add SUBPROCESS_TIMEOUT to mutation_task

--- trust5/tasks/mutation_task.py
+++ trust5/tasks/mutation_task.py
@@ -9,3 +9,4 @@ from stabilize import StageExecution, Task, TaskResult
 from ..core.lang import LanguageProfile
 from ..core.message import M, emit
 logger = logging.getLogger(__name__)
+SUBPROCESS_TIMEOUT = 120
