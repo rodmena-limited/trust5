56400c6b328618d38cc1606ac312bec242c8b2a2
Farshid Ashouri <farsheed.ashouri@gmail.com>
2025-10-03 21:22:16 +0100

    Add _DEFAULT_SKIP_DIRS to compliance

--- trust5/core/compliance.py
+++ trust5/core/compliance.py
@@ -8,3 +8,8 @@ _PASCAL_CASE_RE = re.compile(r"\b[A-Z][a-z]+(?:[A-Z][a-z]+)+\b")
 _BACKTICK_RE = re.compile(r"`([^`]+)`")
 _QUOTED_RE = re.compile(r'"([^"]+)"')
 _SNAKE_CASE_RE = re.compile(r"\b[a-z][a-z0-9]*(?:_[a-z0-9]+)+\b")
+_DEFAULT_SKIP_DIRS = frozenset({
+    "__pycache__", ".venv", "venv", "node_modules", ".git",
+    ".mypy_cache", ".pytest_cache", ".ruff_cache", "dist", "build",
+    ".tox", ".eggs", "*.egg-info",
+})
