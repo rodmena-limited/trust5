9e7e45dedae25337e4223bd0763b445abfca1e33
Farshid Ashouri <farsheed.ashouri@gmail.com>
2025-10-03 21:11:14 +0100

    Add _BACKTICK_RE to compliance

--- trust5/core/compliance.py
+++ trust5/core/compliance.py
@@ -5,3 +5,4 @@ import re
 from dataclasses import dataclass
 logger = logging.getLogger(__name__)
 _PASCAL_CASE_RE = re.compile(r"\b[A-Z][a-z]+(?:[A-Z][a-z]+)+\b")
+_BACKTICK_RE = re.compile(r"`([^`]+)`")
