a79c423e001815b40c4fb10ec59ac2dbdf2947d3
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-11 19:20:53 +0000

    Add RegressionConfig, DDDConfig, TDDConfig to config

--- trust5/core/config.py
+++ trust5/core/config.py
@@ -17,3 +17,21 @@ class SyncGateConfig(BaseModel):
     max_errors: int = 0
     max_warnings: int = 10
     require_clean_lsp: bool = True
+
+class RegressionConfig(BaseModel):
+    error_increase_threshold: int = 0
+    warning_increase_threshold: int = 10
+    type_error_increase_threshold: int = 0
+
+class DDDConfig(BaseModel):
+    require_existing_tests: bool = True
+    characterization_tests: bool = True
+    behavior_snapshots: bool = True
+    max_transformation_size: str = 'medium'
+    preserve_before_improve: bool = True
+
+class TDDConfig(BaseModel):
+    min_coverage_per_commit: int = 80
+    require_test_first: bool = True
+    red_green_refactor: bool = True
+    mutation_testing_enabled: bool = False
