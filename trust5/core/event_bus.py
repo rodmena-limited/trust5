c2c5f0199d2fe4e2b29f35ec106e6323e47173dd
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-11 22:18:16 +0000

    Add K_BLOCK_LINE, K_BLOCK_END, K_STREAM_START to event_bus

--- trust5/core/event_bus.py
+++ trust5/core/event_bus.py
@@ -10,3 +10,6 @@ from dataclasses import dataclass
 logger = logging.getLogger(__name__)
 K_MSG = "msg"  # single-line message
 K_BLOCK_START = "bs"  # block start (label)
+K_BLOCK_LINE = "bl"  # block line (content)
+K_BLOCK_END = "be"  # block end
+K_STREAM_START = "ss"  # stream start (label)
