36d079b40e86df3a8ba07c7d39b46ab37434dfea
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-11 04:39:04 +0000

    Add save, load to token_store

--- trust5/core/auth/token_store.py
+++ trust5/core/auth/token_store.py
@@ -28,3 +28,29 @@ class TokenStore:
         key_path.write_bytes(key)
         key_path.chmod(stat.S_IRUSR | stat.S_IWUSR)
         return key
+
+    def save(self, provider_name: str, token_data: TokenData) -> None:
+        all_tokens = self._load_all()
+        all_tokens[provider_name] = {
+            "access_token": token_data.access_token,
+            "refresh_token": token_data.refresh_token,
+            "expires_at": token_data.expires_at,
+            "token_type": token_data.token_type,
+            "scopes": token_data.scopes,
+            "extra": token_data.extra,
+        }
+        self._save_all(all_tokens)
+
+    def load(self, provider_name: str) -> TokenData | None:
+        all_tokens = self._load_all()
+        data = all_tokens.get(provider_name)
+        if data is None:
+            return None
+        return TokenData(
+            access_token=str(data["access_token"]),
+            refresh_token=str(data["refresh_token"]) if data.get("refresh_token") else None,
+            expires_at=float(data.get("expires_at", 0.0)),
+            token_type=str(data.get("token_type", "Bearer")),
+            scopes=list(data.get("scopes", [])),
+            extra=dict(data.get("extra", {})),
+        )
