4502dfd138398abc3be073e47c17725a10821896
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-11 20:19:03 +0000

    Add _FALLBACK_SKIP_DIRS to context_builder

--- trust5/core/context_builder.py
+++ trust5/core/context_builder.py
@@ -4,3 +4,16 @@ from typing import Any
 MAX_FILE_CONTENT = 6000
 MAX_TOTAL_CONTEXT = 30000
 _FALLBACK_EXTENSIONS = (".py", ".go", ".ts", ".js", ".rs", ".java", ".rb")
+_FALLBACK_SKIP_DIRS = (
+    ".moai",
+    ".trust5",
+    ".git",
+    "node_modules",
+    "vendor",
+    "__pycache__",
+    ".venv",
+    "venv",
+    "target",
+    "dist",
+    "build",
+)
