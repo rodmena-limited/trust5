6af1587a43adcc050a02ce921f3ea2159771d050
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-13 22:06:51 +0000

    Initial message module structure

---
+++ trust5/core/message.py
@@ -0,0 +1,27 @@
+import sys
+import threading
+from datetime import datetime
+from enum import StrEnum
+from .event_bus import (
+    K_BLOCK_END,
+    K_BLOCK_LINE,
+    K_BLOCK_START,
+    K_MSG,
+    K_STREAM_END,
+    K_STREAM_START,
+    K_STREAM_TOKEN,
+    Event,
+    get_bus,
+)
+TOOL_CODE_MAP = {
+    "Bash": M.TBSH,
+    "Read": M.TRED,
+    "Write": M.TWRT,
+    "Edit": M.TEDT,
+    "Glob": M.TGLB,
+    "Grep": M.TGRP,
+    "InstallPackage": M.TPKG,
+    "InitProject": M.TINI,
+    "AskUserQuestion": M.UASK,
+}
+_enabled = True
