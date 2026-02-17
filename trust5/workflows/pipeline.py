5b059cb8b4ee9a1ee617de6e31ddd124e837c545
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-02-17 20:14:21 +0000

    Add MAX_REPAIR_JUMPS to pipeline

--- trust5/workflows/pipeline.py
+++ trust5/workflows/pipeline.py
@@ -4,3 +4,4 @@ from stabilize import StageExecution, TaskExecution, Workflow
 from ..core.config import ConfigManager
 from ..core.lang import detect_language, get_profile
 logger = logging.getLogger(__name__)
+MAX_REPAIR_JUMPS = 50
